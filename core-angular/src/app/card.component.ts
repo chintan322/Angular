b56ead9a18782fd9bafdd4aebd8a226239176d3d
chintan <chintansutariya322@gmail.com>
2020-07-23 12:00:17 +0530

    Core-angular added

---
+++ core-angular/src/app/card.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'card-item',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent {}
